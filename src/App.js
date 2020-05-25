16ecb94696a1c21868b4ffca4408b863033a488e
Sushant <ersushantkm49@gmail.com>
2020-05-25 19:23:45 +0100

    Added custom new component Person

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Learning by Sushant Kumar</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+     return (
+       <div className="App">
+         <h1>Hi I'm a React app</h1>
+         <Person />
+       </div>
+     );
+    //return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'Hi , I \'m react app'))
   }
 }
 
