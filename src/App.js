2651acf745d87ee2084dc51dad465f1aa1b2c3df
Sushant <ersushantkm49@gmail.com>
2020-06-21 23:32:10 +0100

    Radium media query usage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from  'radium';
+import Radium, {StyleRoot} from  'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -94,12 +94,14 @@ class App extends Component {
       }
 
       return (
+        <StyleRoot>
         <div className="App">
          <h1>Hi I'm a React app</h1>
          <p className={classes.join('  ')}>This is really working</p>
          <button style={style} onClick={this.toggerNameHandler}>Toggle names</button>
          {persons}
         </div>
+        </StyleRoot>
      )
     }
     //return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'Hi , I \'m react app'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,12 @@ import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
-    return <div className="Person">
+    const style = {
+        '@media (min-width: 500px)' : {
+            width : '450px'
+        }
+    }
+    return <div className="Person" style={style}>
         <p onClick={props.click}>I'm {props.name} and my age is {props.age}!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} />
